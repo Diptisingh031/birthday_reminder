bd475841d88ce6eb45e2be858febde0817aeddac
DIPTI SINGH <dipti.07519012022@ipu.ac>
2023-08-27 00:30:48 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [birthdays, setBirthdays] = useState([
+    { id: 1, name: 'Dipti', date: '01-01-2004' },
+    { id: 2, name: 'John', date: '01-12-1989' },
+    { id: 3, name: 'Harry', date:'01-12-1999' },
+    { id: 3, name: 'Hari', date:'01-12-1929' },
+    { id: 3, name: 'Jack', date:'12-12-1999' },
+    
+  ]);
+
+
+  const removeBirthday = (id) => {
+    setBirthdays(birthdays.filter((birthday) => birthday.id !== id));
+  };
+
+  useEffect(() => {
+    
+  }, [birthdays]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Birthday Reminder</h1>
+      <div className= "birthday-list">
+      {birthdays.map((birthday) => (
+          <div key={birthday.id} className="birthday-card">
+            <h2>{birthday.name}</h2>
+            <p>{new Date(birthday.date).toDateString()}</p>
+            <button onClick={() => removeBirthday(birthday.id)}>Remove</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
+    
 export default App;
