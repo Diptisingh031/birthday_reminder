aa805390405dd841e6ebfc5aa026b7aac52899d6
DIPTI SINGH <dipti.07519012022@ipu.ac>
2023-09-03 19:07:54 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import {birthdays} from './data';
 
 function App() {
-  const [birthdays, setBirthdays] = useState([
-    { id: 1, name: 'Dipti', date: '01-01-2004' },
-    { id: 2, name: 'John', date: '01-12-1989' },
-    { id: 3, name: 'Harry', date:'01-12-1999' },
-    { id: 3, name: 'Hari', date:'01-12-1929' },
-    { id: 3, name: 'Jack', date:'12-12-1999' },
-    
-  ]);
-
+  const [birthdayData, setBirthdayData] = useState(birthdays);
 
   const removeBirthday = (id) => {
-    setBirthdays(birthdays.filter((birthday) => birthday.id !== id));
+    setBirthdayData(birthdayData.filter((birthday) => birthday.id !== id));
   };
 
   useEffect(() => {
     
-  }, [birthdays]);
+  }, [birthdayData]);
 
   return (
     <div className="App">
       <h1>Birthday Reminder</h1>
       <div className= "birthday-list">
-      {birthdays.map((birthday) => (
+      {birthdayData.map((birthday) => (
           <div key={birthday.id} className="birthday-card">
+            <img src={birthday.imageUrl} alt='NA' />
             <h2>{birthday.name}</h2>
             <p>{new Date(birthday.date).toDateString()}</p>
+            <p>Age: {birthday.age}</p>
             <button onClick={() => removeBirthday(birthday.id)}>Remove</button>
           </div>
         ))}
---
+++ src/data.js
@@ -0,0 +1,40 @@
+// data.js
+
+export const birthdays = [
+    {
+      id: 1,
+      name: 'Preeti',
+      age: '19 years',
+      date: '01-01-2004',
+      imageUrl: 'https://i.pinimg.com/736x/fb/e8/f9/fbe8f92308efe8a97995fd42f535ac9f.jpg',
+    },
+    {
+      id: 2,
+      name: 'John',
+      age: '33 years',
+      date: '01-12-1989',
+      imageUrl: 'https://storage.googleapis.com/pai-images/32351fffd2db499f9c0b0c66dab371a4.jpeg',
+    },
+    { 
+      id: 3,
+      name: 'Harry',
+      age: '24 years' , 
+      date: '01-12-1999',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHN9_h0wdyyd3rHwyhbbd5MrkjitVVrC1G6Q&usqp=CAU',
+    },
+    {
+      id: 4,
+      name: 'Hari',  
+      age: '34 years',
+      date: '01-12-1989' ,
+      imageUrl: 'https://i.pinimg.com/736x/5a/0c/7b/5a0c7b76e2a8bcdbe571c5ba916f93fe.jpg',
+    },
+    { 
+      id: 5,
+      name: 'Jack',
+      age: '24 years', 
+      date: '12-12-1999' ,
+      imageUrl: 'https://image.shutterstock.com/image-vector/anime-little-boy-face-mask-260nw-2007530504.jpg',
+    },
+  ];
+  
